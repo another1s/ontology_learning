fde4778850fecf100ded06a18cc87a4fecc35c31
unknown <164536681@qq.com>
2019-09-06 09:25:06 -0600

    finish iterative training part

--- program/PMF/PMF.py
+++ program/PMF/PMF.py
@@ -1,27 +1,30 @@
 import numpy as np
-from program.PMF.users import fake_users
-
+from program.PMF.users import fake_users, User, UserGroup
+import sqlite3
 
 class PMF:
-    def __init__(self, pretrained_addr=None, userid=None, itemid=None):
+    def __init__(self, pretrained_addr=None, user_set=None, item_set=None):
         self.model = dict()
         if pretrained_addr:
             self.pretrained = self.load_model(pretrained_addr)
-        if userid and itemid:
-            self.userid = userid
-            self.itemid = itemid
+        if user_set and item_set:
+            self.userid = user_set
+            self.itemid = item_set
 
     # U: [num_user, hidden_feature], V:[num_item, hidden_feature]
     def train(self, num_user, num_item, train, test, learning_rate, K, regu_u, regu_i, maxiter, update: 'bool'):
-        if self.model:
-            U = self.model['U']
-            V = self.model['V']
-        elif update is True and self.pretrained:
+        if update is True and self.pretrained:
             U = self.pretrained['U']
             V = self.pretrained['V']
         else:
             U = np.random.normal(0, 0.1, (num_user, K))
             V = np.random.normal(0, 0.1, (num_item, K))
+
+        # U_add = np.random.normal(0, 0.1, (1, K))
+        # V_add = np.random.normal(0, 0.1, (1, K))
+        # U = np.vstack((U, U_add))
+        # V = np.vstack((V, V_add))
+
         pre_rmse = 100.0
         endure_count = 3
         patience = 0
@@ -49,6 +52,42 @@ class PMF:
                 self.save_model(U=U, V=V)
                 break
 
+    # user's behaviour changed or added
+    def iterative_training(self, num_user_added, num_item_added, train, test, learning_rate, K, regu_u, regu_i, maxiter):
+        U = self.model['U']
+        V = self.model['V']
+        U_add = np.random.normal(0, 0.1, (num_user_added, K))
+        V_add = np.random.normal(0, 0.1, (num_item_added, K))
+        U = np.vstack((U, U_add))
+        V = np.vstack((V, V_add))
+        pre_rmse = 100.0
+        endure_count = 3
+        patience = 0
+        for iter in range(maxiter):
+            loss = 0.0
+            for data in train:
+                user = data[0]
+                item = data[1]
+                rating = data[2]
+                predict_rating = np.dot(U[user], V[item].T)
+                error = rating - predict_rating
+                loss += error ** 2
+                U[user] += learning_rate * (error * V[item] - regu_u * U[user])
+                V[item] += learning_rate * (error * U[user] - regu_i * V[item])
+                loss += regu_u * np.square(U[user]).sum() + regu_i * np.square(V[item]).sum()
+            loss = 0.5 * loss
+            rmse = self.eval_rmse(U, V, test)
+            print('iter:%d loss:%.3f rmse:%.5f' % (iter, loss, rmse))
+            if rmse < pre_rmse:  # early stop
+                pre_rmse = rmse
+                patience = 0
+            else:
+                patience += 1
+            if patience >= endure_count:
+                self.save_model(U=U, V=V)
+                break
+
+
     # input: chosen_user(int) , itemid(list)
     # output: result [itemid, predict_rate]
     def recommendbyrank(self, chosen_user, itemid, rank_num):
@@ -75,6 +114,10 @@ class PMF:
         np.savetxt('V.txt', V)
         return self.model
 
+    def load_user_item_set(self, user_set, item_set):
+        self.user_set = user_set
+        self.item_set = item_set
+
     @staticmethod
     def eval_rmse(U, V, test):
         test_count = len(test)
@@ -95,8 +138,15 @@ class PMF:
         model = {'U': U, 'V': V}
         return model
 
+    # @staticmethod
+    # def save_user_behavior(userid, itemid, rate):
+    #     conn = sqlite3.connect('testdata.db')
+    #     cursor = conn.cursor()
+    #     try:
+    #         cursor.execute('''''')
 
-def read_data(path,train_ratio):
+
+def read_data(path, train_ratio):
     user_set = {}
     item_set = {}
     u_idx = 0
@@ -106,6 +156,8 @@ def read_data(path,train_ratio):
         for line in f.readlines():
             u, i, r, _ = line.split('::')
             if u not in user_set:
+                config = {'initial': 'real', 'userid': u}
+                new_user = User(config=config)
                 user_set[u] = u_idx
                 u_idx += 1
             if i not in item_set:
@@ -116,17 +168,34 @@ def read_data(path,train_ratio):
     np.random.shuffle(data)
     train = data[0:int(len(data)*train_ratio)]
     test = data[int(len(data)*train_ratio):]
-    return u_idx, i_idx, train, test
+    return u_idx, i_idx, train, test, user_set, item_set
 
 
 if __name__=='__main__':
-    num_user, num_item, train, test = read_data('program/PMF/data/ratings.dat', 0.8)
+    num_user, num_item, train, test, user_set, item_set = read_data('./data/ratings.dat', 0.8)
     pmf = PMF()
-    pmf.train(num_user, num_item, train, test, 0.01, 10, 0.01, 0.01, 100, False)
-    predict_rate, userid, paperid = pmf.predict([2312, 3])
-    print(predict_rate, ' ', userid, ' ', paperid)
     added_train, train_list = fake_users(user_num=1000, paper_num=5000, instances=3000)
-    #pmf.train(num_user=1000, num_item)
-    #pmf.train(num_user=1000)
+    pmf.load_user_item_set(user_set, item_set)
+    pmf.train(num_user, num_item, train, test, 0.01, 10, 0.01, 0.01, 100, False)
+    user_added = 0
+    item_added = 0
+    for element in train_list:
+        u = element[0]
+        v = element[1]
+        r = element[2]
+        if u not in user_set:
+            user_added = user_added + 1
+        if v not in item_set:
+            item_added = item_added + 1
+
+    #for example in train_list:
+    predict_rate, userid, paperid = pmf.predict([1, 3])
+    print(predict_rate, ' ', userid, ' ', paperid)
+
+
+    pmf.iterative_training(num_user_added=user_added, num_item_added=item_added, train=train_list, test=test,
+                           learning_rate=0.01, K=10, regu_u=0.01, regu_i=0.01, maxiter=100)
+    predict_rate, userid, paperid = pmf.predict([1, 3])
+    print(predict_rate, ' ', userid, ' ', paperid)
 
 
--- program/PMF/users.py
+++ program/PMF/users.py
@@ -1,29 +1,46 @@
 import numpy as np
+import sqlite3
 
-
-class Users:
+class User:
     def __init__(self, config):
         self.initial = config['initial']
         self.userid = config['userid']
         self.preference = list()
+        # self.save_to_database(self.userid, self.initial)
 
     # action: 0 for indifferent, 1 for viewed , 2 for interested
     @staticmethod
     def behaviour(paperid, userid, action):
         return np.array([paperid, userid, action])
 
+    # @staticmethod
+    # def save_to_database(user_id, user_type):
+    #     conn = sqlite3.connect('../testdata.db')
+    #     cursor = conn.cursor()
+    #     user_id = int(user_id)
+    #     try:
+    #         cursor.execute("INSERT INTO users (userid, usertype) VALUES (?, ?)", (user_id, user_type))
+    #         conn.commit()
+    #         conn.close()
+    #     except:
+    #         cursor.execute('''CREATE TABLE IF NOT EXISTS users (userid text, usertype text)''')
+    #         cursor.execute("INSERT INTO users (userid, usertype) VALUES (?, ?)", (user_id, user_type))
+    #         conn.commit()
+    #         conn.close()
 
-class UserGroup(Users):
+class UserGroup(User):
     def __init__(self, user_num, config_list):
         self.user_list = list()
         for i in range(user_num):
-            user = Users.__init__(self, config=config_list[i])
+            user = User.__init__(self, config=config_list[i])
             self.user_list.append(user)
 
 
 def fake_users(user_num, paper_num, instances):
     user_id = np.random.random_integers(low=0, high=user_num, size=instances)
+    user_set = set(user_id)
     paper_id = np.random.random_integers(low=0, high=paper_num, size=instances)
+    item_set = set(paper_id)
     a = paper_id.T
     user_paper = np.vstack((user_id, paper_id.T))
     action = np.random.random_integers(low=0, high=2, size=instances)
@@ -34,7 +51,5 @@ def fake_users(user_num, paper_num, instances):
     return data, list(data)
 
 
-res = ['wew', 'ewew', 'weww1']
-print(str(res))
 # res1, res2 = fake_users(3000, 30000, 12000)
 # print(1)
