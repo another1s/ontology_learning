6544c52dae07beaf94f95f85487bc2d14b78fccc
unknown <164536681@qq.com>
2019-09-06 09:41:50 -0600

    finish iterative training part

--- program/PMF/PMF.py
+++ program/PMF/PMF.py
@@ -174,7 +174,7 @@ def read_data(path, train_ratio):
 if __name__=='__main__':
     num_user, num_item, train, test, user_set, item_set = read_data('./data/ratings.dat', 0.8)
     pmf = PMF()
-    added_train, train_list = fake_users(user_num=1000, paper_num=5000, instances=3000)
+    added_train, train_list = fake_users(user_num=3, paper_num=120, instances=1200)
     pmf.load_user_item_set(user_set, item_set)
     pmf.train(num_user, num_item, train, test, 0.01, 10, 0.01, 0.01, 100, False)
     user_added = 0
